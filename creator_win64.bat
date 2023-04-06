7262076ddb03ae7ed1ef85ed691d1e054d8b71cf
Alex Lauderdale <tzolkat@gmail.com>
2023-04-06 09:59:09 -0400

    Build environment customization for win11x64.

--- creator_win64.bat
+++ creator_win64.bat
@@ -13,14 +13,23 @@ if not errorlevel 1 (
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
         popd
     ) else (
-        echo You do not appear to have Visual Studio 2015 or 2017 installed.
-        exit /b 1
+        reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.17.0" >> nul 2>&1
+        if not errorlevel 1 (
+            rem :: VS2022
+            rem :: NOTE: The instructions contained in the NMakefile are no longer necessary with this method.
+            pushd "C:\Program Files\Microsoft Visual Studio\2022\Community\"
+            call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+            popd
+        ) else (
+            echo "You do not appear to have Visual Studio 2015, 2017, or 2022 installed."
+            exit /b 1
+        )
     )
 ) 
 
 if not exist C:\flexandbison\ ( echo "You need to have the Flex and Bison Packages, please see documentation." && exit /b 1 )
 
-set PATH=%PATH%;C:\flexandbison
+set PATH=%PATH%;E:\DevTools\FlexAndBison
 nmake /F NMakefile
 mkdir binary\windows64\
 move lslint.exe binary\windows64\
