7237692cd62282500e882198c9883378b3270005
Alex Lauderdale <tzolkat@gmail.com>
2023-07-14 20:53:10 -0400

    Download builtins.txt from sgd internal kwdb branch.

--- create_builtins.sh
+++ create_builtins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl https://raw.githubusercontent.com/ShadowGear-SL/kwdb/dev/outputs/builtins.txt -o builtins.txt
 echo "Rebuilding builtins_txt.cc..."
 echo "const char *builtins_txt[] = {" > builtins_txt.cc
 sed -e '/^\/\//d; s/"/\\\"/g; s/^/"/; s/$/",/' builtins.txt >> builtins_txt.cc || { rm -f builtins_txt.cc ; false ; }
